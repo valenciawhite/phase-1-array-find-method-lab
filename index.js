cb11f152afc4aeb878fc6870064f73abb1e53e54
Valencia White <valenciawhite@rocketmail.com>
2022-02-28 05:20:26 -0500

    commit tree 2abaef3ef9b076c51376f6966fae0c7d1c71713c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+  function superbowlWin(collection){
+      for (const record of collection){
+          if (record.result === "W"){
+              console.log(record.year);
+              return record.year;
+          }
+      }
+  }
